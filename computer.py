c2d64975a1f907cb1ff674c07a0873ec23f8a207
k-alasgarova <kalasgarova@smith.edu>
2026-02-10 14:46:16 -0500

    version 1

--- computer.py
+++ computer.py
@@ -4,7 +4,44 @@ class Computer:
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
+    
+    def update_price(self, new_price):
+        self.price = new_price
+
+    def update_os(self, new_os):
+        self.operating_system = new_os
+
+    def refurbish(self, new_os=None):
+        if int(self.year_made) < 2000:
+            self.price = 0
+        elif int(self.year_made) < 2012:
+            self.price = 250
+        elif int(self.year_made) < 2018:
+            self.price = 550
+        else:
+            self.price = 1000
+        
+        if new_os is not None:
+            self.operating_system = new_os
+        
+    def __str__(self):
+        return(
+            f"{self.description} -- "
+            f"Processor Type: {self.processor_type}"
+            f"Hard Drive Capacity: {self.hard_drive_capacity}"
+            f"Memory: {self.memory}"
+            f"Operating System: {self.operating_system}"
+            f"Year Made: {self.year_made}"
+            f"Price: {self.price}"
+        )
+
 
     # What methods will you need?
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,37 @@
 class ResaleShop:
 
-    # What attributes will it need?
 
-    # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self):
+        self.inventory = []
+    
+    def buy(self, computer):
+        self.inventory.append(computer)
+    
+    def sell(self, computer):
+        if computer in self.inventory:
+            self.inventory.remove(computer)
+        else:
+            print("Computer not found. Please select another item to sell.")
+        
+    def update_price(self, computer, new_price):
+        if computer in self.inventory:
+            computer.update_price(new_price)
+        else:
+            print("Computer not found. Cannot update price.")
+        
+    def refurbish(self, computer, new_os=None):
+        if computer in self.inventory:
+            computer.refurbish(new_os)
+        else:
+            print("Computer not found. Please select another item to refurbish.")
+    
+    def print_inventory(self):
+        if self.inventory:
+            for item in self.inventory:
+                print(item)
+            else:
+                print("No inventory to display.")
+
 
     # What methods will you need?
